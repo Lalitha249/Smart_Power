49c72876ac70f48dd254367ec75019afb13bc29b
Lalitha M <lalithacse249@gmail.com>
2025-12-04 19:53:24 +0530

    Delete Backend/ML directory

--- Backend/ML/reward_system.py
+++
@@ -1,8 +0,0 @@
-def calculate_rewards(total_used, plan_units):
-    """
-    Reward system:
-    - If user uses < 80% of limit → reward 10 points
-    """
-    if total_used <= 0.80 * plan_units:
-        return 10
-    return 0
