10c24d352e75dc0fdc5370df7e2ee2cfcb491ce0
Lalitha M <lalithacse249@gmail.com>
2026-01-01 18:24:15 +0530

    Delete Backend/db directory

--- Backend/db/mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URI = "mongodb+srv://admin:Password%4025@smartpowercluster.felsaoi.mongodb.net/?retryWrites=true&w=majority&appName=SmartPowerCluster"
-
-client = MongoClient(MONGO_URI)
-
-db = client["SmartPowerDB"]   # You can rename this
